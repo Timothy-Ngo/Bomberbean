102d9f78d130fd15c46eb65969558a46a7267c17
Timothy Ngo <77961406+Timothy-Ngo@users.noreply.github.com>
2023-04-20 16:56:24 -0700

    Main Menu

---
+++ Bomberbean/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+public class ButtonController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else   
+            Application.Quit();
+        #endif
+    }
+}
