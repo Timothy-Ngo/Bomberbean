284ced36a648df45dae5fe6a7ef7a4428f2115f5
Timothy Ngo <77961406+Timothy-Ngo@users.noreply.github.com>
2023-05-03 02:16:31 -0700

    Win Game
    - Stats screen added
    - Fixed extra bomb bug

---
+++ Bomberbean/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ScoreController : MonoBehaviour
+{
+    // public UIController ui;
+    // public BombController bc;
+    // public Player player1;
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    
+}
